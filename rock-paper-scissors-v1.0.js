49749092536228f4a05ebbaecd4901a38b5c7d10
Evgeniy Prikhodko <eprikhodkodev@gmail.com>
2020-12-11 22:54:42 +0300

    Add first nice version of rock-paper-scissors game

---
+++ rock-paper-scissors-v1.0.js
@@ -0,0 +1,48 @@
+// declare an array of game items
+const items = ["Rock", "Paper", "Scissors"];
+
+// ask user for input and make it lowercase
+let playerSelection = prompt("rock, paper, or scissors? Which one you choose?").toLowerCase();
+
+// capitalize first letter of user input string
+playerSelection = capitalizeFirstLetter(playerSelection);
+
+function capitalizeFirstLetter(playerSelection) {
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+}
+
+// log an item choosed by player to the console
+console.log(playerSelection + "(Player)");
+
+// declare computerSelection variable
+let computerSelection = computerPlay();
+
+//random item selecting by computer from an [items] array
+function computerPlay() {
+  const randomItem = Math.floor(Math.random() * items.length);
+  return items[randomItem];
+}
+
+// log an item choosed by computer to the console
+console.log(computerSelection + "(PC)");
+
+// play round of a game
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw!";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win! Scissors beats Paper";
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
--- test-script.js
+++ test-script.js
@@ -1,60 +1,56 @@
-//declare an array of hands
-const hands = ["Rock", "Paper", "Scissors"];
+// declare an array of game items
+const items = ["Rock", "Paper", "Scissors"];
 
-//user input part
-let playerSelection = prompt("rock, paper, or scissors? Which one you choose?");
-playerSelection = properOutput();
+// play game 5 times
+function game() {
+  for (let i = 0; i < 5; i++) {
 
-function properOutput() {
-//make user input lowercase
-playerSelection = playerSelection.toLowerCase();
+    // ask user for input and make it lowercase
+    let playerSelection = prompt("rock, paper, or scissors? Which one you choose?").toLowerCase();
 
-//capitalize first letter
-function capitalizeFirstLetter(playerSelection) {
-  return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-}
+    // capitalize first letter of user input string
+    playerSelection = capitalizeFirstLetter(playerSelection);
 
-playerSelection = capitalizeFirstLetter(playerSelection);
-  return playerSelection;
-}
+    function capitalizeFirstLetter(playerSelection) {
+        return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    }
 
-console.log(playerSelection + "(Player)");
+    // log an item choosed by player to the console
+    console.log(playerSelection + "(Player)");
+    
+    // declare computerSelection variable
+    let computerSelection = computerPlay();
 
-let computerSelection = computerPlay();
+    //random item selecting by computer from an [items] array
+    function computerPlay() {
+      const randomItem = Math.floor(Math.random() * items.length);
+      return items[randomItem];
+    }
 
-//random hands selecting by computer
-function computerPlay() {
-  const randomHand = Math.floor(Math.random() * hands.length);
-  return hands[randomHand];
-}
+    // log an item choosed by computer to the console
+    console.log(computerSelection + "(PC)");
+    // play round of a game
+    function playRound(playerSelection, computerSelection) {
+      if (playerSelection === computerSelection) {
+        return "Draw!";
+      } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock";
+      } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors";
+      } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Win! Paper beats Rock";
+      } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper";
+      } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors";
+      } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beats Paper";
+      }
+    }
 
-console.log(computerSelection + "(PC)");
-
-//play round of a game
-function playRound(playerSelection, computerSelection) {
-  // compare input from user and input from computer.
-  // if  playerSelection === computerSelection return string "Draw!"
-  if (playerSelection === computerSelection) {
-    return "Draw!";
-  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    return "You Lose! Paper beats Rock";
-  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    return "You Win! Rock beats Scissors";
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    return "You Win! Paper beats Rock";
-  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    return "You Lose! Scissors beats Paper";
-  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    return "You Lose! Rock beats Scissors";
-  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    return "You Win! Scissors beats Paper";
+    console.log(playRound(playerSelection, computerSelection));
   }
-  // else if playerSelection === "Rock" && computerSelection === "Paper" return string "You Lose! Paper beats Rock"
-  // else if playerSelection === "Rock" && computerSelection === "Scissors" return string "You Win! Rock beats Scissors"
-  // else if playerSelection === "Paper" && computerSelection === "Rock" return string "You Win! Paper beats Rock"
-  // else if playerSelection === "Paper" && computerSelection === "Scissors" return string "You Lose! Scissors beats Paper"
-  // else if playerSelection === "Scissors" && computerSelection === "Rock" return string "You Lose! Rock beats Scissors"
-  // else if playerSelection === "Scissors" && computerSelection === "Paper" return string "You Win! Scissors beats Paper"
+
 }
 
-console.log(playRound(playerSelection, computerSelection));
+game();
