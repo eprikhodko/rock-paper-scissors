c4094513b8cc59e77cd25b80819e48689cbb80ea
Evgeniy Prikhodko <eprikhodkodev@gmail.com>
2020-12-11 19:36:30 +0300

    Add new code for a rock-paper-scissors.

--- test-script-v1.1.js
+++ test-script-v1.1.js
@@ -1,11 +1,15 @@
 //declare an array of hands
 const hands = ["Rock", "Paper", "Scissors"];
 
-//user input part
-let playerSelection = prompt("rock, paper, or scissors? Which one you choose?");
-playerSelection = properOutput();
+//random hands selecting by computer
+function computerPlay() {
+  const randomHand = Math.floor(Math.random() * hands.length);
+  return hands[randomHand];
+}
 
-function properOutput() {
+//ask user for input
+let playerSelection = prompt("rock, paper, or scissors? Which one you choose?");
+
 //make user input lowercase
 playerSelection = playerSelection.toLowerCase();
 
@@ -15,39 +19,13 @@ function capitalizeFirstLetter(playerSelection) {
 }
 
 playerSelection = capitalizeFirstLetter(playerSelection);
-  return playerSelection;
-}
-
-console.log(playerSelection + "(Player)");
-
-let computerSelection = computerPlay();
-
-//random hands selecting by computer
-function computerPlay() {
-  const randomHand = Math.floor(Math.random() * hands.length);
-  return hands[randomHand];
-}
-
-console.log(computerSelection + "(PC)");
 
 //play round of a game
 function playRound(playerSelection, computerSelection) {
   // compare input from user and input from computer.
   // if  playerSelection === computerSelection return string "Draw!"
-  if (playerSelection === computerSelection) {
+  if playerSelection === computerSelection {
     return "Draw!";
-  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    return "You Lose! Paper beats Rock";
-  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    return "You Win! Rock beats Scissors";
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    return "You Win! Paper beats Rock";
-  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    return "You Lose! Scissors beats Paper";
-  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    return "You Lose! Rock beats Scissors";
-  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    return "You Win! Scissors beats Paper";
   }
   // else if playerSelection === "Rock" && computerSelection === "Paper" return string "You Lose! Paper beats Rock"
   // else if playerSelection === "Rock" && computerSelection === "Scissors" return string "You Win! Rock beats Scissors"
@@ -57,4 +35,5 @@ function playRound(playerSelection, computerSelection) {
   // else if playerSelection === "Scissors" && computerSelection === "Paper" return string "You Win! Scissors beats Paper"
 }
 
+const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
--- test-script-v1.2.js
+++ test-script-v1.2.js
@@ -1,25 +1,6 @@
 //declare an array of hands
 const hands = ["Rock", "Paper", "Scissors"];
 
-//user input part
-let playerSelection = prompt("rock, paper, or scissors? Which one you choose?");
-playerSelection = properOutput();
-
-function properOutput() {
-//make user input lowercase
-playerSelection = playerSelection.toLowerCase();
-
-//capitalize first letter
-function capitalizeFirstLetter(playerSelection) {
-  return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-}
-
-playerSelection = capitalizeFirstLetter(playerSelection);
-  return playerSelection;
-}
-
-console.log(playerSelection + "(Player)");
-
 let computerSelection = computerPlay();
 
 //random hands selecting by computer
@@ -28,7 +9,25 @@ function computerPlay() {
   return hands[randomHand];
 }
 
-console.log(computerSelection + "(PC)");
+//user input part
+let playerSelection = userInput();
+
+//ask user for input
+function userInput() {
+  let playerSelection = prompt("rock, paper, or scissors? Which one you choose?");
+
+  //make user input lowercase
+  playerSelection = playerSelection.toLowerCase();
+
+  //capitalize first letter
+  function capitalizeFirstLetter(playerSelection) {
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+  }
+
+//return playerSelection value
+  playerSelection = capitalizeFirstLetter(playerSelection);
+  return playerSelection;
+}
 
 //play round of a game
 function playRound(playerSelection, computerSelection) {
@@ -36,18 +35,6 @@ function playRound(playerSelection, computerSelection) {
   // if  playerSelection === computerSelection return string "Draw!"
   if (playerSelection === computerSelection) {
     return "Draw!";
-  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    return "You Lose! Paper beats Rock";
-  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    return "You Win! Rock beats Scissors";
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    return "You Win! Paper beats Rock";
-  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    return "You Lose! Scissors beats Paper";
-  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    return "You Lose! Rock beats Scissors";
-  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    return "You Win! Scissors beats Paper";
   }
   // else if playerSelection === "Rock" && computerSelection === "Paper" return string "You Lose! Paper beats Rock"
   // else if playerSelection === "Rock" && computerSelection === "Scissors" return string "You Win! Rock beats Scissors"
