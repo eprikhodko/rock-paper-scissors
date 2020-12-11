b6b32c7bb8fb9139bcc509c07dffe82204ec0f1a
Evgeniy Prikhodko <eprikhodkodev@gmail.com>
2020-12-11 10:34:40 +0300

    Add playerSelection

--- test-script.js
+++ test-script.js
@@ -1,6 +1,22 @@
+//declare an array of hands
 const hands = ["Rock", "Paper", "Scissors"];
 
+//random hands selecting by computer
 function computerPlay() {
   const randomHand = Math.floor(Math.random() * hands.length);
   return hands[randomHand];
 }
+
+//ask user for input
+let playerSelection = prompt("rock, paper, or scissors? Which one you choose?");
+//make user input lowercase
+playerSelection = playerSelection.toLowerCase();
+
+//play round of a game
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
